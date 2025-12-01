2529b641a6274a6b49434fe3587fe689daeabe6f
Pree Simphliphan <prees26@bu.edu>
2025-11-30 20:43:08 -0500

    Gazebo for robotics arm

--- workspace/src/mvp_robot/setup.py
+++ workspace/src/mvp_robot/setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         # (os.path.join('share', package_name, 'srv'), glob('srv/*.srv')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
