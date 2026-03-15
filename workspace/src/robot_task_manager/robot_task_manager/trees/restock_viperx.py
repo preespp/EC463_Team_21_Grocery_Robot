7acd31236f617e7df65c0105fe7fac7b532b9c63
Pree Simphliphan <prees26@bu.edu>
2026-03-15 06:20:31 -0400

    Update

--- workspace/src/robot_task_manager/robot_task_manager/trees/restock_viperx.py
+++ workspace/src/robot_task_manager/robot_task_manager/trees/restock_viperx.py
@@ -1,15 +1,11 @@
 import py_trees
 import robot_task_manager.bt_nodes as bt_nodes
-from robot_task_manager.bt_nodes.navigation_nodes import MoveDistanceForCurrentItem
 
 
 def create_restock_viperx_tree(bb):
 
     process_one_item = py_trees.composites.Sequence("ProcessRestockItem", memory=True, children=[
         bt_nodes.SetCurrentItem(bb),
-        ### Delete Later Only for Feature 1 Demo Need to Migrate to real auto nav script)
-        MoveDistanceForCurrentItem(bb),
-        ###############################################
 
         # bt_nodes.NavigateToGoalPose(goal_key="nav_goal"),
         # bt_nodes.RepositionViperXArm(goal_key="basket_pose"),
@@ -23,10 +19,6 @@ def create_restock_viperx_tree(bb):
         #         bt_nodes.VerifyViperXPosition(),
         #     ]),
         # ),
-        # bt_nodes.RepositionViperXArm(goal_key="home_pose"),
-
-        # bt_nodes.RepositionRackToGoalLevel(goal_key="rack_goal"),
-
         # bt_nodes.RepositionViperXArm(goal_key="shelf_pose"),
         # bt_nodes.VerifyViperXPosition(),
         # py_trees.decorators.Retry(
@@ -39,7 +31,6 @@ def create_restock_viperx_tree(bb):
         # ),
         # bt_nodes.MoveViperXGripper(command="open"),
         # bt_nodes.RepositionViperXArm(goal_key="home_pose"),
-        # bt_nodes.RepositionRackToGoalLevel(goal_key=1),
 
         bt_nodes.ChangeInventory(bb, mode="restock"),
     ])
